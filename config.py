9d5cdbfa4f6c6bfd0c77bc79d47f6a929e3b4583
forexreviews-fxr <114882067+forexreviews-fxr@users.noreply.github.com>
2022-10-04 09:45:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
 
-API_ID = 10899585            # Your Telegram API_ID , Get It from my.telegram.org
-API_HASH = "41b93f8d5b001664e138489fbddb3f09"       # Your Telegram API_HASH , Get It from my.telegram.org
-SESSION_STRING="BACmUIEACyxPYzLiUP70ctHBwnkbuOqSgeluKuj7De8X5PlI1HKVyQwL_hWClHAWZBx4ASslyqo0T1oa8mFTovEX7oXq1s8PaN4lyvheFDVDegXe5P2dsuw_YV-mp3Y4TzYtwbuFbUVdj5AStfDC6jH3-n0fRshtVQriI96vqWqXpBfh0Oc6Mx0KJlzHlJhFHoEYWmLwjuxObjMxDpC1CLlbObN8RBnzGmrYLp6eghMQbMo38y5j-j_KuQwwnqsAav1XavuyJt9xfNYVnalDy0FqCJ0ef21F2O2-fAVFcGY292xeJkaOBWr3TNnxoupDmt-VyMiZ506oiRSdqmtpNVsAPdl2vgAAAAAHWAs9AA"    # Your Telegram Account Session String. Run generate string session to generate
+API_ID = 144            # Your Telegram API_ID , Get It from my.telegram.org
+API_HASH = "f49c31726d444e8"       # Your Telegram API_HASH , Get It from my.telegram.org
+SESSION_STRING="BQDgXIAAD0e0WeWsVB1fLces9uGLDb6XkMxtOUFJQRvCPGEpEbG5l2bHCR1_n5jaiPwe9IuJ5GVyNRAElOaGzinI3rZAPeN5QXtyxBUhzhtfAr2F1jt8o17y3W7HQIVw9sLKPWmFdag-4_-QHSf6P3ipytmBscBCc4NgHUwcAS2cMv-_yuaWupFa5ARFIHd-SIggVUeNqDoxBPPjnFESvPI4rpVkKSNwCicfXpNqlpe"    # Your Telegram Account Session String. Run generate string session to generate
 
 
 
